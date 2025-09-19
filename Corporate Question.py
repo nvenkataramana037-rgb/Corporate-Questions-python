b9f435bdc32b0caedc4ef7e7b1d4a988b4dc911a
nvenkataramana037 <nvenkataramana037@gmail.com>
2025-09-19 15:46:43 +0530

    Add files via upload

---
+++ Corporate Question.py
@@ -0,0 +1,51 @@
+'''
+#Sum of all cubes from n-m
+m = int(input("enter starting value"))
+n = int(input("enter ending value"))
+
+if m > n:
+    print(0)
+else:
+    total_sum = 0
+    for i in range(m, n +1):
+        total_sum += i ** 3
+    print(total_sum) 
+
+#products of all num from n-m
+m = int(input("enter starting value"))
+n = int(input("enter ending value"))
+
+if m > n:
+    print(0)
+else:
+    total_sum = 0
+    for i in range(m, n +1):
+        total_sum += i*i
+    print(total_sum) 
+
+
+#product of all even num from n-m
+m = int(input("enter starting value"))
+n = int(input("enter ending value"))
+
+if m > n:
+    print(0)
+else:
+    total_sum = 0
+    for i in range(m, n +1):
+        if (i%2==0):
+            total_sum += i * i
+    print(total_sum)
+    '''
+#product of all odd num from n-m
+m = int(input("enter starting value"))
+n = int(input("enter ending value"))
+
+if m > n:
+    print(0)
+else:
+    total_sum = 0
+    for i in range(m, n +1):
+        if (i%2!=0):
+            total_sum += i * i
+    print(total_sum)
